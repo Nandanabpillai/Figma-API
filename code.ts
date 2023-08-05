dc5c5d6d4da4f7be1322e0b3f6806b5cec34a86b
Nandana B Pillai <90125324+Nandanabpillai@users.noreply.github.com>
2023-08-06 03:51:31 +0530

    Add files via upload

--- code.ts
+++ code.ts
@@ -8,59 +8,74 @@ figma.ui.onmessage = msg => {
     const parentFrame = figma.createFrame()
     parentFrame.x = -500
     parentFrame.y = -500
-    parentFrame.resize(1222, 1933)
-
+    parentFrame.resize(1222, 986)
+    
     const rect1 = figma.createRectangle()
-    rect1.cornerRadius = 4
-    // const myFontLoadingFunction = async () => {
-    //   await figma.loadFontAsync({ family: "Work Sans", style: "Bold" })
-    // }
-    
-    // // Another function where we use the loaded font
-    // const myAnotherFunction = () => {
-    //   const myTextLayer = figma.createText()
-    //   myTextLayer.fontName = { family: "Work Sans", style: "Bold" }
-    //   myTextLayer.textCase = "UPPER"
-    //   myTextLayer.characters = "This is my title"
-    //   figma.currentPage.appendChild(myTextLayer);
-    // }
-    
-    // myFontLoadingFunction().then(() => {
-    //   myAnotherFunction()
-    // })
-
+    rect1.cornerRadius = 8
     rect1.x = -400
-    rect1.y = -418
-    rect1.resize(400, 400)
+    rect1.y = -324
+    rect1.resize(510, 548)
     rect1.fills = [{ type: 'SOLID', color: { r: 0.7, g: 0.9, b: 0.8 } }]
 
+
     const rect2 = figma.createRectangle()
-    rect2.cornerRadius = 4
-    rect2.x = 189
-    rect2.y = -418
-    rect2.resize(400, 400)
+    rect2.cornerRadius = 8
+    rect2.x = 173
+    rect2.y = -324
+    rect2.resize(510, 548)
     rect2.fills = [{ type: 'SOLID', color: { r: 0.6, g: 0.8, b: 0.8 } }]
+    
 
-    const textNode = figma.createText(); // Create a new text node
+    async function loadFonts() {
+      try {
+        await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+        await figma.loadFontAsync({ family: "Inter", style: "Bold" });
+      } catch (error) {
+        console.error("Error loading fonts:", error);
+      }
+    }
+    
+    async function updateTextNode() {
+      // Load the required fonts first
+      await loadFonts();
+    
+      // Now you can update the text node
+      const textNode1 = figma.createText();
+      textNode1.characters = "Movies";
+      textNode1.fontSize = 50;
+      textNode1.x = -229;
+      textNode1.y = -77;
+      figma.currentPage.appendChild(textNode1);
+      figma.currentPage.selection = [textNode1];
+      figma.viewport.scrollAndZoomIntoView([textNode1]);
+      
+      const textNode2 = figma.createText();
+      textNode2.characters = "Books";
+      textNode2.fontSize = 50;
+      textNode2.x = 355;
+      textNode2.y = -77;
+      figma.currentPage.appendChild(textNode2);
+      figma.currentPage.selection = [textNode2];
+      figma.viewport.scrollAndZoomIntoView([textNode2]);
+      
+      const textNode3 = figma.createText();
+      textNode3.characters = "Bucket List";
+      textNode3.fontSize = 65;
+      textNode3.x = -35;
+      textNode3.y = -473;
+      figma.currentPage.appendChild(textNode3);
+      figma.currentPage.selection = [textNode3];
+      figma.viewport.scrollAndZoomIntoView([textNode3]);
 
-    // Set the properties for the text node
-    textNode.characters = "Hello, Figma API!"; // Set the text content
-    textNode.x = 100; // X-coordinate
-    textNode.y = 150; // Y-coordinate
-  
-    // Set other text properties (optional)
-    textNode.fontName = { family: "Roboto", style: "Regular" };
-    textNode.fontSize = 24;
-    textNode.fills = [{ type: 'SOLID', color: { r: 0, g: 0, b: 0 } }];
-  
-    // Add the text node to the current page
-    figma.currentPage.appendChild(textNode);
-  
-    // Select the newly created text node
-    figma.currentPage.selection = [textNode];
-  
-    // Zoom to the selected node
-    figma.viewport.scrollAndZoomIntoView([textNode]);
-    figma.closePlugin("see categories")
+      figma.closePlugin();
+    }
+    
+    // Call the function to update the text node and load fonts
+    updateTextNode();
+    const parentFrame1 = figma.createFrame()
+    parentFrame1.x = -1881
+    parentFrame1.y = -500
+    parentFrame1.resize(1222, 986)
+
   }
 }
